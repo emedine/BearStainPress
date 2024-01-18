c772ccd0f54216d1028d364d6f2987756bbdc40a
emedine <info@ericmedine.com>
2024-01-18 19:38:14 +0100

    added books component

---
+++ photo-gallery/src/components/Books.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+import axios from 'axios';
+
+export class Books extends Component {
+
+   state = {
+
+       books: [],
+
+       isLoaded: false
+
+   }
+
+ componentDidMount () {
+
+   axios.get('https://bearstainpress.com/wp-json/wp/v2/books')
+
+       .then(res => this.setState({
+
+           books: res.data,
+
+           isLoaded: true
+
+       }))
+
+       .catch(err => console.log(err))
+
+   }
+
+   render() {
+
+      console.log(this.state);
+
+       return (
+
+           <div>
+
+              
+
+           </div>
+
+       )
+
+   }
+
+}
